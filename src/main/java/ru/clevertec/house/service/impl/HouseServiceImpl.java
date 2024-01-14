90f951c3de75f8fd018f1c717ce8fdb588cec9a8
Karatkevich Aliaksei <torlesh18112003@gmail.com>
2024-01-14 19:43:23 +0300

    feat: done PersonServiceImpl + logging

--- src/main/java/ru/clevertec/house/service/impl/HouseServiceImpl.java
+++ src/main/java/ru/clevertec/house/service/impl/HouseServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.clevertec.house.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.clevertec.house.dao.impl.HouseDaoImpl;
 import ru.clevertec.house.dto.request.HouseRequest;
@@ -13,47 +14,59 @@ import ru.clevertec.house.service.HouseService;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class HouseServiceImpl implements HouseService {
 
     private final HouseDaoImpl houseDao;
-
     private final HouseMapper houseMapper;
 
     @Override
     public HouseResponse save(HouseRequest houseRequest) {
+        log.info("Saving a new house...");
         House houseToSave = houseMapper.toHouse(houseRequest);
         House savedHouse = houseDao.save(houseToSave);
         HouseResponse response = houseMapper.toResponse(savedHouse);
+        log.info("House saved successfully. UUID: {}", response.uuid());
         return response;
     }
 
     @Override
     public HouseResponse findByUuid(UUID uuid) {
+        log.info("Finding house by UUID: {}", uuid);
         HouseResponse response = houseDao.findByUuid(uuid)
                 .map(houseMapper::toResponse)
-                .orElseThrow(() -> NotFoundException.of(House.class, uuid));
+                .orElseThrow(() -> {
+                    log.warn("House not found with UUID: {}", uuid);
+                    return NotFoundException.of(House.class, uuid);
+                });
+        log.info("House found successfully. UUID: {}", response.uuid());
         return response;
     }
 
     @Override
     public List<HouseResponse> findAll(int pageNumber, int pageSize) {
+        log.info("Finding all houses. Page number: {}, Page size: {}", pageNumber, pageSize);
         List<HouseResponse> houses = houseDao.findAll(pageNumber, pageSize).stream()
                 .map(houseMapper::toResponse)
                 .toList();
-
+        log.info("Found {} houses.", houses.size());
         return houses;
     }
 
     @Override
     public void update(UUID uuid, HouseRequest houseRequest) {
+        log.info("Updating house with UUID: {}", uuid);
         House houseToUpdate = houseMapper.toHouse(houseRequest);
         houseDao.update(uuid, houseToUpdate);
+        log.info("House updated successfully. UUID: {}", uuid);
     }
 
     @Override
     public void delete(UUID uuid) {
+        log.info("Deleting house with UUID: {}", uuid);
         houseDao.delete(uuid);
+        log.info("House deleted successfully. UUID: {}", uuid);
     }
 }
--- src/main/java/ru/clevertec/house/service/impl/PersonServiceImpl.java
+++ src/main/java/ru/clevertec/house/service/impl/PersonServiceImpl.java
@@ -1,38 +1,73 @@
 package ru.clevertec.house.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.clevertec.house.dao.impl.PersonDaoImpl;
 import ru.clevertec.house.dto.request.PersonRequest;
 import ru.clevertec.house.dto.response.PersonResponse;
+import ru.clevertec.house.entity.Person;
+import ru.clevertec.house.exception.NotFoundException;
+import ru.clevertec.house.mapper.PersonMapper;
 import ru.clevertec.house.service.PersonService;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class PersonServiceImpl implements PersonService {
+
+    private final PersonDaoImpl personDao;
+
+    private final PersonMapper personMapper;
+
     @Override
     public PersonResponse save(PersonRequest personRequest) {
-        return null;
+        log.info("Saving a new person...");
+        Person personToSave = personMapper.toPerson(personRequest);
+        Person savedPerson = personDao.save(personToSave);
+        PersonResponse response = personMapper.toResponse(savedPerson);
+        log.info("Person saved successfully. UUID: {}", response.uuid());
+        return response;
     }
 
     @Override
     public PersonResponse findByUuid(UUID uuid) {
-        return Optional.empty();
+        log.info("Finding person by UUID: {}", uuid);
+        PersonResponse response = personDao.findByUuid(uuid)
+                .map(personMapper::toResponse)
+                .orElseThrow(() -> {
+                    log.warn("Person not found with UUID: {}", uuid);
+                    return NotFoundException.of(Person.class, uuid);
+                });
+        log.info("Person found successfully. UUID: {}", response.uuid());
+        return response;
     }
 
     @Override
     public List<PersonResponse> findAll(int pageNumber, int pageSize) {
-        return null;
+        log.info("Finding all persons. Page number: {}, Page size: {}", pageNumber, pageSize);
+        List<PersonResponse> persons = personDao.findAll(pageNumber, pageSize).stream()
+                .map(personMapper::toResponse)
+                .toList();
+        log.info("Found {} persons.", persons.size());
+        return persons;
     }
 
     @Override
     public void update(UUID uuid, PersonRequest personRequest) {
-
+        log.info("Updating person with UUID: {}", uuid);
+        Person personToUpdate = personMapper.toPerson(personRequest);
+        personDao.update(uuid, personToUpdate);
+        log.info("Person updated successfully. UUID: {}", uuid);
     }
 
     @Override
     public void delete(UUID uuid) {
-
+        log.info("Deleting person with UUID: {}", uuid);
+        personDao.delete(uuid);
+        log.info("Person deleted successfully. UUID: {}", uuid);
     }
 }
